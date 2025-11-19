d5c85ec4c2754bb75ce5705ea257bc9b59bc5fa3
Muhammad Arif <muhammad.arif3108@gmail.com>
2025-11-19 10:24:06 +0700

    update layout home

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -387,7 +387,7 @@
         /* Product Image */
         .product-image {
             width: 100%;
-            height: 200px;
+            height: 100%;
             object-fit: cover;
             transition: transform 0.3s ease;
         }
